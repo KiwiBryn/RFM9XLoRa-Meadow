e3afe770226654a149559670a4a6c33b2466e495
Bryn Lewis <bryn.lewis@devmobile.co.nz>
2020-01-03 14:29:27 +1300

    First working version of RFM9X/SX127X library for Wilderness Labs Meadow

---
+++ Rfm9XLoRaDevice/RegisterManager.cs
@@ -0,0 +1,112 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) August 2018, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.Rfm9x
+{
+	using System;
+	using System.Diagnostics;
+	using System.Runtime.InteropServices.WindowsRuntime;
+
+	using Meadow.Hardware;
+
+	public sealed class RegisterManager
+	{
+		private IDigitalOutputPort ChipSelectGpioPin = null;
+		private SpiPeripheral Rfm9XLoraModem = null;
+		private const byte RegisterAddressReadMask = 0X7f;
+		private const byte RegisterAddressWriteMask = 0x80;
+
+		public RegisterManager(IIODevice device, ISpiBus spiBus, IPin chipSelectPin)
+		{
+			// Chip select pin configuration
+			ChipSelectGpioPin = device.CreateDigitalOutputPort(chipSelectPin, initialState: true);
+
+			Rfm9XLoraModem = new SpiPeripheral(spiBus, ChipSelectGpioPin);
+		}
+
+		public Byte ReadByte(byte address)
+		{
+			byte[] writeBuffer = new byte[] { address &= RegisterAddressReadMask };
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			byte[] readBuffer = Rfm9XLoraModem.WriteRead(writeBuffer, 2);
+
+			return readBuffer[1];
+		}
+
+		public ushort ReadWord(byte address)
+		{
+			byte[] writeBuffer = new byte[] { address &= RegisterAddressReadMask };
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			byte[] readBuffer = Rfm9XLoraModem.WriteRead(writeBuffer, 3);
+
+			return (ushort)(readBuffer[2] + (readBuffer[1] << 8));
+		}
+
+		public byte[] Read(byte address, int length)
+		{
+			byte[] writeBuffer = new byte[] { address &= RegisterAddressReadMask };
+			byte[] repyBuffer = new byte[length + 1];
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			byte[] readBuffer = Rfm9XLoraModem.WriteRead(writeBuffer, (ushort)repyBuffer.Length);
+
+			Array.Copy(readBuffer, 1, repyBuffer, 0, length);
+
+			return repyBuffer;
+		}
+
+		public void WriteByte(byte address, byte value)
+		{
+			byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, value };
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			Rfm9XLoraModem.WriteBytes(writeBuffer);
+		}
+
+		public void WriteWord(byte address, ushort value)
+		{
+			byte[] valueBytes = BitConverter.GetBytes(value);
+			byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, valueBytes[0], valueBytes[1] };
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			Rfm9XLoraModem.WriteBytes(writeBuffer);
+		}
+
+		public void Write(byte address, [ReadOnlyArray()] byte[] bytes)
+		{
+			byte[] writeBuffer = new byte[1 + bytes.Length];
+			Debug.Assert(Rfm9XLoraModem != null);
+
+			Array.Copy(bytes, 0, writeBuffer, 1, bytes.Length);
+			writeBuffer[0] = address |= RegisterAddressWriteMask;
+
+			Rfm9XLoraModem.WriteBytes(writeBuffer);
+		}
+
+		public void Dump(byte start, byte finish)
+		{
+			Console.WriteLine("Register dump");
+			for (byte registerIndex = start; registerIndex <= finish; registerIndex++)
+			{
+				byte registerValue = this.ReadByte(registerIndex);
+
+				Console.WriteLine("Register 0x{0:x2} - Value 0X{1:x2} - Bits {2}", registerIndex, registerValue, Convert.ToString(registerValue, 2).PadLeft(8, '0'));
+			}
+		}
+	}
+}
---
+++ Rfm9xLoRaDeviceClient/MeadowApp.cs
@@ -0,0 +1,103 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) January 2020, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.Rfm9x.LoRaDeviceClient
+{
+   using System;
+   using System.Diagnostics;
+   using System.Text;
+   using System.Threading.Tasks;
+
+   using Meadow;
+   using Meadow.Devices;
+   using Meadow.Hardware;
+
+   public class MeadowApp : App<F7Micro, MeadowApp>
+   {
+      private const double Frequency = 915000000.0;
+      private byte MessageCount = Byte.MaxValue;
+      private Rfm9XDevice rfm9XDevice;
+
+      public MeadowApp()
+      {
+         try
+         {
+            ISpiBus spiBus = Device.CreateSpiBus(500);
+
+            rfm9XDevice = new Rfm9XDevice(Device, spiBus, Device.Pins.D09, Device.Pins.D10, Device.Pins.D12);
+
+            rfm9XDevice.Initialise(Frequency, paBoost: true, rxPayloadCrcOn: true);
+#if DEBUG
+            rfm9XDevice.RegisterDump();
+#endif
+
+            rfm9XDevice.OnReceive += Rfm9XDevice_OnReceive;
+#if ADDRESSED_MESSAGES_PAYLOAD
+            rfm9XDevice.Receive(UTF8Encoding.UTF8.GetBytes("LoRaIoT1"));
+#else
+            rfm9XDevice.Receive();
+#endif
+            rfm9XDevice.OnTransmit += Rfm9XDevice_OnTransmit;
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+
+         Task.Delay(10000).Wait();
+
+         while (true)
+         {
+            string messageText = string.Format("Hello from {0} ! {1}", Environment.MachineName, MessageCount);
+            MessageCount -= 1;
+
+            byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(messageText);
+            Console.WriteLine("{0:HH:mm:ss}-TX {1} byte message {2}", DateTime.Now, messageBytes.Length, messageText);
+#if ADDRESSED_MESSAGES_PAYLOAD
+            this.rfm9XDevice.Send(UTF8Encoding.UTF8.GetBytes("AddressHere"), messageBytes);
+#else
+            this.rfm9XDevice.Send(messageBytes);
+#endif
+            Task.Delay(30000).Wait();
+         }
+      }
+
+      private void Rfm9XDevice_OnReceive(object sender, Rfm9XDevice.OnDataReceivedEventArgs e)
+      {
+         try
+         {
+            string messageText = UTF8Encoding.UTF8.GetString(e.Data);
+
+#if ADDRESSED_MESSAGES_PAYLOAD
+            string addressText = UTF8Encoding.UTF8.GetString(e.Address);
+
+            Console.WriteLine(@"{0:HH:mm:ss}-RX From {1} PacketSnr {2:0.0} Packet RSSI {3}dBm RSSI {4}dBm = {5} byte message ""{6}""", DateTime.Now, addressText, e.PacketSnr, e.PacketRssi, e.Rssi, e.Data.Length, messageText);
+#else
+            Console.WriteLine(@"{0:HH:mm:ss}-RX PacketSnr {1:0.0} Packet RSSI {2}dBm RSSI {3}dBm = {4} byte message ""{5}""", DateTime.Now, e.PacketSnr, e.PacketRssi, e.Rssi, e.Data.Length, messageText);
+#endif
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+      }
+
+      private void Rfm9XDevice_OnTransmit(object sender, Rfm9XDevice.OnDataTransmitedEventArgs e)
+      {
+         Console.WriteLine("{0:HH:mm:ss}-TX Done", DateTime.Now);
+      }
+   }
+}
